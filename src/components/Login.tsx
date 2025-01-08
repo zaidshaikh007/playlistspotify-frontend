deddbf4afa559e816fc0267300a74d7553a7ab35
mitesh-solanki <16mscit072@gmail.com>
2025-01-08 21:03:08 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -45,7 +45,9 @@ const Login = () => {
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" onSubmit={objForm.handleSubmit(onSubmit)}>
               <div>
+                <div className="flex items-center justify-between">
                 <label className="block text-sm/6 font-medium text-gray-900">Email address</label>
+                </div>
                 <div className="mt-2">
                   <input type="email" id="email" {...objForm.register("email")} required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                 </div>
